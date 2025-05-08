24475ed70b576d84410dfa830b5774d7512bffcd
Liamvanbart1 <90309539+Liamvanbart1@users.noreply.github.com>
2025-05-08 05:16:14 +0200

    readme alleen nog

--- JS/opdracht.js
+++ JS/opdracht.js
@@ -1,97 +1,57 @@
-// ik wil een bij het laden van de pagina een functie starten //
-
-
-
-// class met display none en deze toevoegen / weghalen als de browser javascript heeft //
-
-// https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage //
-
-// https://developer.mozilla.org/en-US/docs/Web/API/FormData
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // document.addEventListener("DOMContentLoaded", function () {
-//     const form = document.querySelector("form");
-//
-//     // Functie om formuliergegevens op te slaan in localStorage
-//     function saveFormData() {
-//         const formData = {};
-//         new FormData(form).forEach((value, key) => {
-//             formData[key] = value;
+//   const form = document.querySelector("form");
+
+//   // Functie om formuliergegevens op te slaan in localStorage
+//   function saveFormData() {
+//     const formData = {};
+//     new FormData(form).forEach((value, key) => {
+//       formData[key] = value;
+//     });
+//     localStorage.setItem("formData", JSON.stringify(formData));
+//     console.log("Form data opgeslagen:", formData);
+//   }
+
+//   // Functie om formuliergegevens te laden uit localStorage
+//   function loadFormData() {
+//     const savedData = localStorage.getItem("formData");
+//     if (savedData) {
+//       const formData = JSON.parse(savedData);
+//       Object.keys(formData).forEach((key) => {
+//         const elements = document.querySelectorAll(`[name="${key}"]`);
+//         if (!elements.length) return;
+
+//         elements.forEach((input) => {
+//           if (input.type === "radio" || input.type === "checkbox") {
+//             if (input.value === formData[key]) {
+//               input.checked = true;
+//             }
+//           } else {
+//             input.value = formData[key];
+//           }
 //         });
-//         localStorage.setItem("formData", JSON.stringify(formData));
-//         console.log("Form data opgeslagen:", formData);
+//       });
+//       console.log("Form data geladen:", formData);
 //     }
-//
-//     // Functie om formuliergegevens te laden uit localStorage
-//     function loadFormData() {
-//         const savedData = localStorage.getItem("formData");
-//         if (savedData) {
-//             const formData = JSON.parse(savedData);
-//             Object.keys(formData).forEach(key => {
-//                 const input = document.querySelector(`[name="${key}"]`);
-//                 if (input) {
-//                     if (input.type === "radio") {
-//                         document.querySelector(`[name="${key}"][value="${formData[key]}"]`).checked = true;
-//                     } else {
-//                         input.value = formData[key];
-//                     }
-//                 }
-//             });
-//             console.log("Form data geladen:", formData);
-//         }
-//     }
-//
-//     // Formulier opslaan elke 60 seconden
-//     setInterval(saveFormData, 6000);
-//
-//     // Formulier laden bij paginalaad
-//     loadFormData();
+//   }
+
+//   // Formulier opslaan elke 60 seconden
+//   setInterval(saveFormData, 6000); // 60 seconden, niet 600ms
+
+//   // Formulier laden bij paginalaad
+//   loadFormData();
 // });
+document.addEventListener("DOMContentLoaded", () => {
+  const nextToTab2 = document.getElementById("nextToTab2");
+  const nextToTab3 = document.getElementById("nextToTab3");
 
+  const tab2Radio = document.getElementById("tab2");
+  const tab3Radio = document.getElementById("tab3");
 
+  nextToTab2?.addEventListener("click", () => {
+    tab2Radio.checked = true;
+  });
+
+  nextToTab3?.addEventListener("click", () => {
+    tab3Radio.checked = true;
+  });
+});
